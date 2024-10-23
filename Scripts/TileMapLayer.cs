1f05c6e0098ab172faa5daee0c61740015d1aff9
Levente <v.levente1999@gmail.com>
2024-10-23 15:08:44 +0200

    Add AStarGrid2D for pathfinding

--- Scripts/TileMapLayer.cs
+++ Scripts/TileMapLayer.cs
@@ -1,12 +1,25 @@
 using Godot;
-using System;
 
 public partial class TileMapLayer : Godot.TileMapLayer
 {
+	AStarGrid2D grid;
+
+	Vector2I cellSize = new Vector2I(16, 16);
+
+	Vector2I start = new Vector2I(0, 0);
+	Vector2I end = new Vector2I(5, 2);
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("TileMap dimensions: ", GetUsedRect().Size);
+		grid = new AStarGrid2D();
+		grid.Region = GetUsedRect();
+		grid.CellSize = cellSize;
+		grid.Offset = cellSize / 2;
+		grid.DiagonalMode = AStarGrid2D.DiagonalModeEnum.Never;
+		grid.Update();
+
+		GD.Print("TileMap rect: ", GetUsedRect());
 		GD.Print("TileMap position: ", Position);
 		GD.Print("Top left tile position: ", ToGlobal(MapToLocal(GetUsedRect().Position)));
 	}
@@ -14,5 +27,47 @@ public partial class TileMapLayer : Godot.TileMapLayer
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+    public override void _Draw()
+    {
+		DrawRect(new Rect2(start * cellSize, cellSize), Colors.GreenYellow);
+    	DrawRect(new Rect2(end * cellSize, cellSize), Colors.OrangeRed);
+
+		for (int x = 0; x < grid.Region.Size.X; x++) {
+			for (int y = 0; y < grid.Region.Size.Y; y++) {
+				Vector2I cell = new Vector2I(x, y) + grid.Region.Position;
+				if (grid.IsPointSolid(cell)) {
+					DrawRect(new Rect2(cell.X * cellSize.X, cell.Y * cellSize.Y, cellSize.X, cellSize.Y), Colors.Aquamarine);
+				}
+			}
+		}
+
+        base._Draw();
+    }
+
+	private void UpdatePath() {
+		//$Line2D.points = new PackedVector2Array(grid.GetPointPath(start, end))
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseButton && mouseButton.Pressed && mouseButton.ButtonIndex == MouseButton.Left) {
+			Vector2I mousePosition = new Vector2I((int)mouseButton.GlobalPosition.X, (int)mouseButton.GlobalPosition.Y);
+			//Vector2I offsetPosition = mousePosition - ;
+			Vector2I cell = mousePosition / cellSize;
+
+			GD.Print("Clisked on cell ", cell);
+
+			if (grid.IsInBoundsv(cell)) {
+				grid.SetPointSolid(cell, !grid.IsPointSolid(cell));
+			}
+			else {
+				GD.Print("Was no in bound");
+			}
+
+			UpdatePath();
+			QueueRedraw();
+		}
 	}
 }
