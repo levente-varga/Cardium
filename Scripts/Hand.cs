edcf7c07886b4b4fa18cad3df14de9994fb43c20
Levente <v.levente1999@gmail.com>
2024-10-24 18:25:27 +0200

    Adjust card positions according to hand size

--- Scripts/Hand.cs
+++ Scripts/Hand.cs
@@ -12,35 +12,51 @@ public partial class Hand : Node2D
 	[Export]
 	public Curve cardRotations;
 
-	int handSize = 0;
+	int handSize = 3;
 
-	const float handWidthRatio = 0.5f;
-	const int handHeight = 100;
+	const float MaxHandWidthRatio = 0.5f;
+	const int MaxHandSize = 10;
+	const int HandHeight = 100;
 
 	public override void _Ready()
 	{
-		PopulateHand(10);
+		PopulateHand();
 	}
 
-	private void PopulateHand(int size)
+	private void PopulateHand()
 	{		
+		foreach (Node child in GetChildren())
+		{
+			RemoveChild(child);
+			child.QueueFree();
+		}
+
 		var cardScene = GD.Load<PackedScene>("res://Scenes/Card.tscn");
 
-		for (int i = 0; i < size; i++)
+
+		float weightStart = (1f - (float)(handSize - 1) / (MaxHandSize - 1)) / 2f;
+
+		GD.Print("Weight start: ", weightStart);
+
+		for (int i = 0; i < handSize; i++)
 		{
-			float weight = (float)i / (size - 1);
-			var card = cardScene.Instantiate<Node2D>();
+			float weight = 0.5f;
+			if (handSize > 1) {
+				weight = weightStart + ((float)i / (MaxHandSize - 1));
+			}
+
+			var card = cardScene.Instantiate<Container>();
 
 			var positionSample = cardHorizontalPositions.Sample(weight);
 			var offsetSample = cardVerticalOffsets.Sample(weight);
 			var rotationSample = cardRotations.Sample(weight);
 
 			card.Position = new Vector2(
-				positionSample * (DisplayServer.WindowGetSize().X * handWidthRatio) + DisplayServer.WindowGetSize().X * handWidthRatio / 2, 
-				DisplayServer.WindowGetSize().Y - handHeight + offsetSample * 100
+				DisplayServer.WindowGetSize().X / 2 + positionSample * MaxHandWidthRatio * DisplayServer.WindowGetSize().X / 2f, 
+				DisplayServer.WindowGetSize().Y - HandHeight + offsetSample * 100
 				);
 
-			card.Rotate(rotationSample * 0.3f);
+			card.Rotation = rotationSample * 0.3f;
 
 			AddChild(card);
 
@@ -48,8 +64,18 @@ public partial class Hand : Node2D
 		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseButton && mouseButton.Pressed && mouseButton.ButtonIndex == MouseButton.Left)
+		{
+			if (handSize < 10) handSize++;
+			PopulateHand();
+		}
+
+		if (@event is InputEventMouseButton mouseButton2 && mouseButton2.Pressed && mouseButton2.ButtonIndex == MouseButton.Right)
+		{
+			if (handSize > 1) handSize--;
+			PopulateHand();
+		}
+    }
 }
