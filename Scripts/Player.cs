0f33e66e07a8b9b17948e32670692b6624a05287
Levente <v.levente1999@gmail.com>
2025-02-19 22:37:33 +0100

    Add player attributes

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,8 +7,17 @@ public partial class Player : Sprite2D
 	private Deck _combatDeck = new();
 	private Deck _actionDeck = new();
 	private Pile _discardPile = new();
+	private Hand _hand = new();
 	
-	// Called when the node enters the scene tree for the first time.
+	private int _maxHealth = 5;
+	private int _health = 5;
+	private int _maxEnergy = 5;
+	private int _energy = 5;
+	private int _armor = 0;
+	private int _attack = 1;
+	private int _luck = 0;
+	
+	
 	public override void _Ready()
 	{
 		// Set the player's position to the center of the screen
@@ -18,8 +27,8 @@ public partial class Player : Sprite2D
 		Texture = GD.Load<Texture2D>("res://assets/player.png");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
 }
