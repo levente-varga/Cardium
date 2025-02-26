1e3fd3b8f87a58a75c8da7ac7d2b58949b1a73fa
Levente <v.levente1999@gmail.com>
2025-02-26 21:22:40 +0100

    Add new card sprite

---
+++ Scripts/Global.cs
@@ -0,0 +1,8 @@
+namespace Cardium.Scripts;
+
+public static class Global
+{
+    public const int SpriteSize = 16;
+    public const int Scale = 6;
+    public static int TileSize => SpriteSize * Scale;
+}
