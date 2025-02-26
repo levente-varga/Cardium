969c291fd2cb895afe783bdf97be6867b97e03e1
Levente <v.levente1999@gmail.com>
2025-02-26 17:49:12 +0100

    Add Player movement

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -17,11 +17,13 @@ public partial class Player : Sprite2D
 	private int _attack = 1;
 	private int _luck = 0;
 	
+	private Vector2 _position = Vector2.Zero;
+	
 	
 	public override void _Ready()
 	{
 		// Set the player's position to the center of the screen
-		Position = new Vector2(64, 64);
+		Position = _position * 64;
 
 		// Set the player's texture
 		Texture = GD.Load<Texture2D>("res://assets/player.png");
@@ -29,6 +31,26 @@ public partial class Player : Sprite2D
 
 	public override void _Process(double delta)
 	{
-		
+		// Update the player's position
+		Position = _position * 64;
+	}
+	
+	public override void _Input(InputEvent @event)
+	{
+		switch (@event)
+		{
+			case InputEventKey { Pressed: true, Keycode: Key.Right }:
+				_position.X++;
+				break;
+			case InputEventKey { Pressed: true, Keycode: Key.Left }:
+				_position.X--;
+				break;
+			case InputEventKey { Pressed: true, Keycode: Key.Up }:
+				_position.Y--;
+				break;
+			case InputEventKey { Pressed: true, Keycode: Key.Down }:
+				_position.Y++;
+				break;
+		}
 	}
 }
