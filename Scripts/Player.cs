cb45bda005f476e4b48067e3e89e2fe098303343
Levente <v.levente1999@gmail.com>
2025-02-27 21:47:10 +0100

    Refactor Player

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -11,21 +11,31 @@ public partial class Player : Sprite2D
 	private Pile _discardPile = new();
 	private Hand _hand = new();
 	
-	private int _maxHealth = 5;
-	private int _health = 5;
-	private int _maxEnergy = 5;
-	private int _energy = 5;
-	private int _armor = 0;
-	private int _attack = 1;
-	private int _luck = 0;
+	public int MaxHealth => 5;
+	public int Health { get; private set; } = 5;
+	public int MaxEnergy => 5;
+	public int Energy { get; private set; } = 5;
+	public int Armor => 0;
+	public int Damage => 1;
+	public int Luck => 0;
+	public int Vision => 4;
+	public new Vector2I Position
+	{
+		get => _position;
+		private set
+		{
+			_position = value;
+			base.Position = _position * 64;
+		}
+	}
 	
-	private Vector2I _position = Vector2I.Zero;
+	private Vector2I _position;
 	
 	
 	public override void _Ready()
 	{
 		// Set the player's position to the center of the screen
-		Position = _position * 64;
+		Position = Vector2I.Zero;
 
 		// Set the player's texture
 		Texture = GD.Load<Texture2D>("res://assets/player.png");
@@ -33,8 +43,7 @@ public partial class Player : Sprite2D
 
 	public override void _Process(double delta)
 	{
-		// Update the player's position
-		Position = _position * 64;
+		
 	}
 	
 	public override void _Input(InputEvent @event)
@@ -42,20 +51,20 @@ public partial class Player : Sprite2D
 		switch (@event)
 		{
 			case InputEventKey { Pressed: true, Keycode: Key.Right }:
-				if (World.IsTileEmpty(_position + new Vector2I(1, 0)))
-					_position.X++;
+				if (World.IsTileEmpty(Position + Vector2I.Right))
+					Position += Vector2I.Right;
 				break;
 			case InputEventKey { Pressed: true, Keycode: Key.Left }:
-				if (World.IsTileEmpty(_position + new Vector2I(-1, 0)))
-					_position.X--;
+				if (World.IsTileEmpty(_position + Vector2I.Left))
+					Position += Vector2I.Left;
 				break;
 			case InputEventKey { Pressed: true, Keycode: Key.Up }:
-				if (World.IsTileEmpty(_position + new Vector2I(0, -1)))
-					_position.Y--;
+				if (World.IsTileEmpty(_position + Vector2I.Up))
+					Position += Vector2I.Up;
 				break;
 			case InputEventKey { Pressed: true, Keycode: Key.Down }:
-				if (World.IsTileEmpty(_position + new Vector2I(0, 1)))
-					_position.Y++;
+				if (World.IsTileEmpty(_position + Vector2I.Down))
+					Position += Vector2I.Down;
 				break;
 		}
 	}
