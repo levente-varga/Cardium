6762cd336d4d29954ec1ce3e83871f0be5543ad6
Levente <v.levente1999@gmail.com>
2025-02-27 21:31:23 +0100

    Add SlimeEnemyBehavior

--- Scripts/EnemyBehavior.cs
+++ Scripts/EnemyBehavior.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Godot;
 
 namespace Cardium.Scripts;
@@ -5,9 +7,9 @@ namespace Cardium.Scripts;
 public abstract class EnemyBehavior
 {
     public abstract int MaxHealth { get; }
-    public abstract int Health { get; }
+    public int Health { get; protected set;  }
     public abstract int MaxEnergy { get; }
-    public abstract int Energy { get; }
+    public int Energy { get; protected set; }
     public abstract int Armor { get; }
     public abstract int Damage { get; }
     public abstract int Range { get; }
@@ -15,8 +17,25 @@ public abstract class EnemyBehavior
     public abstract string Name { get; }
     public abstract string Description { get; }
     public abstract Texture2D Sprite { get; }
+    public List<Card> Inventory = new();
+    
+    public delegate void OnDeathDelegate(EnemyBehavior enemy);
+    public event OnDeathDelegate OnDeathEvent;
 
     public abstract void OnTurn(Player player);
-    public abstract void OnDamaged(Player player, int damage);
+
+    public virtual void OnDamaged(Player player, int damage)
+    {
+        Health -= Math.Min(1, damage - Armor);
+        if (Health < 0) OnDeath(player);
+        
+    }
+    
     public abstract void OnTargeted(Player player);
+    
+    public virtual void OnDeath(Player player)
+    {
+        Health = 0;
+        OnDeathEvent?.Invoke(this);
+    }
 }
