6ada5c3367ce8543c28acb585c815388321ff94e
Levente <v.levente1999@gmail.com>
2025-03-02 03:48:10 +0100

    Make bonfire inactive by default

--- Scripts/Interactables/Bonfire.cs
+++ Scripts/Interactables/Bonfire.cs
@@ -4,11 +4,13 @@ namespace Cardium.Scripts.Interactables;
 
 public partial class Bonfire : Interactable
 {
+    bool active = false;
+    
     public override void _Ready()
     {
         base._Ready();
 
-        SetAnimation("idle", GD.Load<Texture2D>("res://Assets/Animations/Bonfire.png"), 4, 12);
+        SetStillFrame(GD.Load<Texture2D>("res://Assets/Sprites/Bonfire.png"));
     }
 
     public override void _Process(double delta)
@@ -20,6 +22,11 @@ public partial class Bonfire : Interactable
     {
         base.OnInteract(source);
         
-        SpawnFloatingLabel("Restored!", color: new Color("F4B41B"));
+        if (active) return;
+        
+        active = true;
+        
+        SetAnimation("idle", GD.Load<Texture2D>("res://Assets/Animations/Bonfire.png"), 4, 12);
+        SpawnFloatingLabel("Restored!", color: new Color("ffffff"));
     }
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -23,7 +23,7 @@ public partial class Player : Entity
 
 		HealthBar.Visible = false;
 
-		SetStillFrame(GD.Load<Texture2D>("res://assets/player.png"));
+		SetStillFrame(GD.Load<Texture2D>("res://Assets/Sprites/player.png"));
 	}
 
 	public override void _Process(double delta)
