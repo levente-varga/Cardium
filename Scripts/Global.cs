412bdb0c287b378f144ba2484264916fc5a1cc66
Levente <v.levente1999@gmail.com>
2025-03-02 16:31:30 +0100

    Rework colors

--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -16,4 +16,5 @@ public static class Global
     public static readonly Color Yellow = new Color("F4B41B");
     public static readonly Color Red = new Color("E6482E");
     public static readonly Color White = new Color("FFFFFF");
+    public static readonly Color Magenta = new Color("472d3c");
 }
