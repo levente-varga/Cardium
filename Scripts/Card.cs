e34129494ad17a8d2fb8451b58cc3d4b8d70b9ee
Levente <v.levente1999@gmail.com>
2025-03-06 14:16:46 +0100

    Add card dragging

--- Scripts/Card.cs
+++ Scripts/Card.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 namespace Cardium.Scripts;
@@ -24,34 +25,61 @@ public partial class Card : Node2D
 	public CardRarity Rarity { get; protected set; }
 	public Sprite2D Art { get; protected set; }
 
+	private bool _dragging;
+	
 	private Tween _hoverTween;
 	
+	private Node2D _base;
 	private Node2D _body;
 	private Sprite2D _sprite;
-	private Control _hitbox;
+	private Button _hitbox;
 
 	public override void _Ready()
 	{
+		ZIndex = 1;
+		
 		_sprite = new Sprite2D();
 		_sprite.Centered = true;
 		_sprite.Texture = GD.Load<Texture2D>("res://Assets/Sprites/Cards/Card.png");
-		_sprite.Scale = new Vector2(6, 6);
+		_sprite.Scale = new Vector2(Global.CardScale, Global.CardScale);
 		
-		_hitbox = new Control();
+		_hitbox = new Button();
+		_hitbox.Flat = true;
 		_hitbox.MouseEntered += OnMouseEntered;
 		_hitbox.MouseExited += OnMouseExited;
-		_hitbox.Size = _sprite.Texture.GetSize() * 6;
-		_hitbox.Position = -_sprite.Texture.GetSize() * 6 / 2;
+		_hitbox.Pressed += OnMousePressed;
+		_hitbox.ButtonDown += OnMouseDown;
+		_hitbox.ButtonUp += OnMouseUp;
+		_hitbox.Size = _sprite.Texture.GetSize() * Global.CardScale;
+		_hitbox.Position = -_sprite.Texture.GetSize() * Global.CardScale / 2;
+		_hitbox.AddThemeStyleboxOverride("normal", new StyleBoxEmpty());
+		_hitbox.AddThemeStyleboxOverride("pressed", new StyleBoxEmpty());
+		_hitbox.AddThemeStyleboxOverride("hover", new StyleBoxEmpty());
+		_hitbox.AddThemeStyleboxOverride("disabled", new StyleBoxEmpty());
+		_hitbox.AddThemeStyleboxOverride("focus", new StyleBoxEmpty());
+		_hitbox.FocusMode = Control.FocusModeEnum.None;
 		
 		_body = new Node2D();
 		_body.AddChild(_hitbox);
 		_body.AddChild(_sprite);
-		AddChild(_body);
+		
+		_base = new Node2D();
+		_base.AddChild(_body);
+		AddChild(_base);
 	}
 
 	public override void _Process(double delta)
 	{
-		
+		if (_dragging)
+		{
+			_body.GlobalPosition = _body.GlobalPosition.Lerp(GetGlobalMousePosition(), Global.LerpWeight * (float)delta);
+			_body.GlobalRotation = Mathf.Lerp(_body.GlobalRotation, 0, (float)delta);
+		}
+		else
+		{
+			_body.Position = _body.Position.Lerp(Vector2.Zero, Global.LerpWeight * (float)delta);
+			_body.Rotation = Mathf.Lerp(_body.Rotation, 0, Global.LerpWeight * (float)delta);
+		}
 	}
 
 	public virtual void OnPlay(Player player) {}
@@ -60,35 +88,54 @@ public partial class Card : Node2D
 	public virtual void OnDiscard(Player player) {}
 	public virtual void OnDrawn(Player player) {}
 	public virtual void OnDestroy(Player player) {}
-	
 
-	public void OnMouseEntered() {
-		if (_hoverTween != null)
-		{
-			_hoverTween.Kill();
-			_hoverTween.Dispose();
-		}
-		
+	private void ResetHoverTween()
+	{
+		if (_hoverTween == null) return;
+		_hoverTween.Kill();
+		_hoverTween.Dispose();
+	}
+
+	private void PlayHoverAnimation()
+	{
+		ResetHoverTween();	
 		_hoverTween = CreateTween();
-		_hoverTween.TweenProperty(_body, "position", new Vector2(0, -100), 0.4f)
+		_hoverTween.TweenProperty(_base, "position", new Vector2(0, -100), 0.4f)
 			.SetEase(Tween.EaseType.Out)
 			.SetTrans(Tween.TransitionType.Expo);
+	}
+	
+	private void PlayUnhoverAnimation()
+	{
+		ResetHoverTween();
+		_hoverTween = CreateTween();
+		_hoverTween.TweenProperty(_base, "position", Vector2.Zero, 0.4f)
+			.SetEase(Tween.EaseType.Out)
+			.SetTrans(Tween.TransitionType.Expo);
+	}
+
+	public void OnMouseEntered() {
+		if (_dragging) return;
+		PlayHoverAnimation();
 	}
 
 	public void OnMouseExited() {
-		if (_hoverTween != null)
-		{
-			_hoverTween.Kill();
-			_hoverTween.Dispose();
-		}
-		
-		_hoverTween = CreateTween();
-		_hoverTween.TweenProperty(_body, "position", Vector2.Zero, 0.4f)
-			.SetEase(Tween.EaseType.Out)
-			.SetTrans(Tween.TransitionType.Expo);
+		if (_dragging) return;
+		PlayUnhoverAnimation();
 	}
 	
 	public void OnMousePressed() {
-		//_animationPlayer.Play("Play");
+		
+	}
+	
+	public void OnMouseDown() {
+		_dragging = true;
+		ZIndex = 2;
+	}
+	
+	public void OnMouseUp() {
+		PlayUnhoverAnimation();
+		_dragging = false;
+		ZIndex = 1;
 	}
 }
--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -6,6 +6,7 @@ public static class Global
 {
     public static readonly Vector2I SpriteSize = new (16, 16);
     public const int Scale = 4;
+    public const int CardScale = 6;
     public static Vector2I TileSize => SpriteSize * Scale;
     public const float LerpWeight = 15f;
     
