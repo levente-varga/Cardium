7ae40f26c391ccb27f0fcc115976ead3b5511437
Levente <v.levente1999@gmail.com>
2025-04-10 16:10:14 +0200

    Rework ChainCard

--- Scripts/Cards/ChainCard.cs
+++ Scripts/Cards/ChainCard.cs
@@ -1,4 +1,5 @@
-using System.Linq;
+using System;
+using System.Collections.Generic;
 using Cardium.Scripts.Cards.Types;
 using Godot;
 
@@ -9,6 +10,8 @@ public partial class ChainCard : EnemyTargetingCard
     public int Damage { get; set; } = 2;
     public int BounceRange { get; set; } = 2;
     public int Bounces { get; set; } = 2;
+
+    private Random _random = new();
     
     public ChainCard()
     {
@@ -22,18 +25,30 @@ public partial class ChainCard : EnemyTargetingCard
 
     public override bool OnPlay(Player player, Enemy enemy, World world)
     {
-        var otherEnemies = world.GetEnemiesInRange(BounceRange, enemy.Position);
+        var enemies = world.GetEnemiesInRange(BounceRange, enemy.Position);
+        enemies.Remove(enemy);
 
-        for (var i = 0; i < otherEnemies.Count && otherEnemies.Count < Bounces - 1; i++)
-        {
-            otherEnemies.AddRange(world.GetEnemiesInRange(BounceRange, otherEnemies[i].Position).Where(e => !otherEnemies.Contains(e)));
+        void HitEnemies(int bouncesLeft, Enemy current, List<Enemy> candidates, List<Enemy> hit) {
+            if (bouncesLeft <= 0) return;
+            
+            bouncesLeft--;
+            current.ReceiveDamage(player, Damage);
+            hit.Add(current);
+
+            if (bouncesLeft <= 0) return;
+            
+            var otherEnemies = world.GetEnemiesInRange(BounceRange, current.Position);
+            otherEnemies.RemoveAll(e => hit.Contains(e) || candidates.Contains(e));
+            candidates.AddRange(otherEnemies);
+            
+            if (candidates.Count == 0) return;
+            
+            var next = candidates[_random.Next(candidates.Count)];
+            candidates.Remove(next);
+            HitEnemies(bouncesLeft, next, candidates, hit);
         }
         
-        enemy.ReceiveDamage(player, Damage);
-        for (var i = 0; i < otherEnemies.Count && i < Bounces; i++)
-        {
-            otherEnemies[i].ReceiveDamage(player, Damage);
-        }
+        HitEnemies(Bounces, enemy, enemies, new());
 
         return true;
     }
