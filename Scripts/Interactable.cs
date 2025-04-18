25902b6178a4e160a02ab20d451cd731c9c57604
Levente <v.levente1999@gmail.com>
2025-04-18 19:03:46 +0200

    Add Bonfire State

--- Scripts/Interactable.cs
+++ Scripts/Interactable.cs
@@ -1,26 +1,18 @@
-using Godot;
-
 namespace Cardium.Scripts;
 
-public partial class Interactable : TileAlignedGameObject
-{
-    private bool _solid = true;
-    public bool Solid
-    {
-        get => _solid;
-        protected set
-        {
-            _solid = value;
-            OnSolidityChangeEvent?.Invoke(this, _solid);
-        }
+public partial class Interactable : TileAlignedGameObject {
+  private bool _solid = true;
+  public bool Solid {
+    get => _solid;
+    protected set {
+      _solid = value;
+      OnSolidityChangeEvent?.Invoke(this, _solid);
     }
-    public bool Interacted = false;
-    
-    public delegate void OnSolidityChangeDelegate(Interactable interactable, bool solid);
-    public event OnSolidityChangeDelegate? OnSolidityChangeEvent;
-    
-    public virtual void OnInteract(Player player, Camera camera)
-    {
-        Blink();
-    }
+  }
+  public bool Interacted = false;
+
+  public delegate void OnSolidityChangeDelegate(Interactable interactable, bool solid);
+  public event OnSolidityChangeDelegate? OnSolidityChangeEvent;
+
+  public virtual void OnInteract(Player player, Camera camera) => Blink();
 }
--- Scripts/Interactables/Bonfire.cs
+++ Scripts/Interactables/Bonfire.cs
@@ -2,37 +2,41 @@ using Godot;
 
 namespace Cardium.Scripts.Interactables;
 
-public partial class Bonfire : Interactable
-{
-    public override void _Ready()
-    {
-        base._Ready();
+public partial class Bonfire : Interactable {
+  public enum BonfireState {
+    Unlit,
+    Lit,
+    Extinguished,
+  }
 
-        SetStillFrame(GD.Load<Texture2D>("res://Assets/Sprites/Bonfire.png"));
-    }
+  public BonfireState State { get; private set; } = BonfireState.Unlit;
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
-    }
+  public override void _Ready() {
+    base._Ready();
 
-    public override void OnInteract(Player player, Camera camera)
-    {
-        base.OnInteract(player, camera);
-        
-        if (Interacted)
-        {
-            SpawnFloatingLabel("Rested", color: Global.White);
-            if (player.Health == player.MaxHealth) return;
-            var healAmount = player.MaxHealth - player.Health;
-            player.Heal(healAmount);
-            return;
-        }
-        
-        Interacted = true;
-        
-        camera.Shake(25);
-        SetAnimation("idle", GD.Load<Texture2D>("res://Assets/Animations/Bonfire.png"), 4, 12);
-        SpawnFallingLabel("Lit!");
+    SetStillFrame(GD.Load<Texture2D>("res://Assets/Sprites/Bonfire.png"));
+  }
+
+  public override void OnInteract(Player player, Camera camera) {
+    base.OnInteract(player, camera);
+
+    if (State == BonfireState.Extinguished) return;
+    
+    if (State == BonfireState.Lit) {
+      SpawnFloatingLabel("Rested", color: Global.White);
+      if (player.Health == player.MaxHealth) return;
+      var healAmount = player.MaxHealth - player.Health;
+      player.Heal(healAmount);
+      SetStillFrame(GD.Load<Texture2D>("res://Assets/Sprites/Bonfire.png"));
+      Modulate = new Color("333333FF");
+      return;
     }
+    
+    State = BonfireState.Lit;
+    Interacted = true;
+
+    camera.Shake(25);
+    SetAnimation("idle", GD.Load<Texture2D>("res://Assets/Animations/Bonfire.png"), 4, 12);
+    SpawnFallingLabel("Lit!");
+  }
 }
