546df87d6ea896b6321debdc8dba69b56a598f2f
Levente <v.levente1999@gmail.com>
2025-04-18 16:44:54 +0200

    Inherit Pile from Deck

--- Scripts/Deck.cs
+++ Scripts/Deck.cs
@@ -1,62 +1,35 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using Cardium.Scripts.Cards.Types;
-using Godot;
 
 namespace Cardium.Scripts;
 
-public class Deck
-{
-    private readonly List<Card> _cards = new();
-    public List<Card> Cards => new(_cards);
-    
-    public Deck(int capacity = 30) {
-        Capacity = capacity;
-    }
+public class Deck : Pile {
+  public Deck(int capacity = 30) {
+    Capacity = capacity;
+  }
 
-    private int _capacity = 1;
-    public int Capacity { 
-        get => _capacity;
-        private set => _capacity = Math.Max(value, 1);
-    }
+  private int _capacity = 1;
+  public int Capacity { 
+    get => _capacity;
+    private set => _capacity = Math.Max(value, 1);
+  }
 
-    public int Size => _cards.Count;
-    public bool IsEmpty => _cards.Count == 0;
-    public bool IsNotEmpty => _cards.Count > 0;
-    public bool Contains(Card card) => _cards.Contains(card);
+  /// <summary>Adds card to the bottom of the deck.</summary>
+  /// <param name="card">The card to be added.</param>
+  /// <returns>false if the deck is already full.</returns>
+  public override bool Add(Card card) {
+    if (_cards.Count >= Capacity) return false;
+    _cards.Add(card);
+    return true;
+  }
 
-    /// <summary>Adds card to the bottom of the deck.</summary>
-    /// <param name="card">The card to be added.</param>
-    /// <returns>false if the deck is already full.</returns>
-    public bool Add(Card card) {
-        if (_cards.Count >= Capacity) {
-            GD.Print($"Couldn't add card to deck, its full");
-            return false;
-        }
-        GD.Print($"Added a card to the deck");
-        _cards.Add(card);
-        return true;
-    }
-    
-    /// <summary>Removes card from the deck.</summary>
-    /// <param name="card">The card to be removed.</param>
-    /// <returns>false if the card was not in the deck.</returns>
-    public bool Remove(Card card) {
-        return _cards.Remove(card);
-    }
-    
-    public Card? Draw() {
-        if (_cards.Count == 0) return null;
-        
-        GD.Print($"Drawn a card from the deck");
-        
-        var card = _cards.First();
-        Remove(card);
-        return card;
-    }
-    
-    public void Shuffle() {
-        Utils.FisherYatesShuffle(_cards);        
-    }
+  public Card? Draw() {
+    if (IsEmpty) return null;
+    var card = _cards.First();
+    return Remove(card) ? card : null;
+  }
+
+  public void Shuffle() {
+    Utils.FisherYatesShuffle(_cards);        
+  }
 }
--- Scripts/Pile.cs
+++ Scripts/Pile.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 namespace Cardium.Scripts;
 
 public class Pile {
-  private readonly List<Card> _cards = new();
+  protected readonly List<Card> _cards = new();
+  public List<Card> Cards => new(_cards);
   
   public int Size => _cards.Count;
   public bool IsEmpty => _cards.Count <= 0;
   public bool IsNotEmpty => _cards.Count > 0;
 
-  public void Add(Card card) => _cards.Add(card);
+  public virtual bool Add(Card card) {
+    _cards.Add(card);
+    return true;
+  }
   public bool Remove(Card card) => _cards.Remove(card);
   public void Clear() => _cards.Clear();
   public bool Contains(Card card) => _cards.Contains(card);
