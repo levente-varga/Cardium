ee46c615ac809535258a8901707a01e287eb3dbb
Levente <v.levente1999@gmail.com>
2025-04-20 22:32:06 +0200

    Exit the dungeon through Ladder

--- Scripts/Interactables/Ladder.cs
+++ Scripts/Interactables/Ladder.cs
@@ -2,29 +2,30 @@ using Godot;
 
 namespace Cardium.Scripts.Interactables;
 
-public partial class Ladder : Interactable
-{
-    public override void _Ready()
-    {
-        base._Ready();
+public partial class Ladder : Interactable {
+  private int _lastPlayerTurnsLived = -2;
+  
+  public override void _Ready() {
+    base._Ready();
 
-        SetStillFrame(ResourceLoader.Load<Texture2D>("res://Assets/Sprites/Ladder.png"));
-    }
+    SetStillFrame(ResourceLoader.Load<Texture2D>("res://Assets/Sprites/Ladder.png"));
+  }
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
-    }
+  public override void OnNudge(Player player, Camera camera) {
+    base.OnNudge(player, camera);
 
-    public override void OnInteract(Player player, Camera camera)
-    {
-        base.OnInteract(player, camera);
-        
-        if (Interacted) return;
-        
-        Interacted = true;
-        Solid = false;
-        camera.Shake(10);
-        SpawnFallingLabel("Climbed!");
+    if (player.TurnsLived - _lastPlayerTurnsLived == 1) {
+      Data.Level = Level.Lobby;
+      
+      // TODO: Save cards
+      
+      GetTree().ReloadCurrentScene();
+      return;
     }
+    
+    _lastPlayerTurnsLived = player.TurnsLived;
+    
+    camera.Shake(10);
+    SpawnFloatingLabel("Leave dungeon?");
+  }
 }
