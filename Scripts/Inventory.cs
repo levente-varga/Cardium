cabb057939d820f11c089359e68685e6e6860bac
Levente <v.levente1999@gmail.com>
2025-04-24 18:48:10 +0200

    Expand inventory with a stash page

--- Scripts/Inventory.cs
+++ Scripts/Inventory.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Cardium.Scripts.Cards.Types;
 using Godot;
@@ -10,16 +9,15 @@ public enum DraggedCardState { None, OverDeckArea, OverInventoryArea, OverStashA
 
 public partial class Inventory : Control {
 	[Export] public Player Player = null!;
-	[Export] public VBoxContainer DeckContainer = null!;
+	[Export] public VBoxContainer StashContainer = null!;
 	[Export] public VBoxContainer InventoryContainer = null!;
-	[Export] public Control DeckArea = null!;
+	[Export] public VBoxContainer DeckContainer = null!;
+	[Export] public Control StashArea = null!;
 	[Export] public Control InventoryArea = null!;
+	[Export] public Control DeckArea = null!;
 	[Export] public Button OkButton = null!;
 	
 	[Export] private PackedScene _cardScene = ResourceLoader.Load<PackedScene>("res://Scenes/card.tscn");
-
-	private readonly List<CardView> _cardsInInventory = new();
-	private readonly List<CardView> _cardsInDeck = new();
 	
 	private DraggedCardState _draggedCardState;
 	
@@ -53,11 +51,12 @@ public partial class Inventory : Control {
 	private void FillContainersWithCardViews() {
 		List<Card> cardsInUse = new(Player.Deck.Deck.GetCards());
 		
-		FillContainerWithCardViews(DeckContainer, cardsInUse, _cardsInDeck);
-		FillContainerWithCardViews(InventoryContainer, Player.Inventory.GetCards(), _cardsInInventory);
+		FillContainerWithCardViews(StashContainer, Data.Stash.GetCards());
+		FillContainerWithCardViews(InventoryContainer, Player.Inventory.GetCards());
+		FillContainerWithCardViews(DeckContainer, cardsInUse);
 	}
 
-	private void FillContainerWithCardViews(Container container, List<Card> cards, List<CardView> storage) {
+	private void FillContainerWithCardViews(Container container, List<Card> cards) {
 		foreach (var child in container.GetChildren()) child?.QueueFree();
 		HBoxContainer row = null!;
 		for (var i = 0; i < cards.Count; i++) {
@@ -80,34 +79,13 @@ public partial class Inventory : Control {
 			view.OnDragStartEvent += OnCardDragStartEventHandler;
 			view.OnDragEndEvent += OnCardDragEndEventHandler;
 			view.OnDragEvent += OnCardDragEventHandler;
-			//view.OnPressedEvent += OnCardPressedEventHandler;
 			cardContainer.AddChild(view);
 			row.AddChild(cardContainer);
 			
-			storage.Add(view);
-
 			if (i != cards.Count - 1 && rowNumber == (i + 1) / CardsPerRow) continue;
 			row.SetCustomMinimumSize(new Vector2(Global.CardSize.X * CardsPerRow, Global.CardSize.Y));
 			container.AddChild(row);
 			row = new HBoxContainer();
-		}
-	}
-
-	private void OnCardPressedEventHandler(CardView view) {
-		if (_cardsInInventory.Contains(view)) {
-			// TODO: Move dragged card to deck
-			Player.Inventory.Remove(view.Card);
-			Player.Deck.Add(view.Card);
-			FillContainersWithCardViews();
-			GD.Print($"Moved {view.Card.Name}: Inventory -> Deck");
-		}
-		else if (_cardsInDeck.Contains(view)) {
-			// TODO: Move dragged card to inventory
-			Player.Deck.Remove(view.Card);
-			Player.Inventory.Add(view.Card);
-			FillContainersWithCardViews();
-			GD.Print($"Moved {view.Card.Name}: Deck -> Inventory");
-
 		}
 	}
 	
@@ -128,7 +106,7 @@ public partial class Inventory : Control {
 	private void OnCardDragEventHandler(CardView view, Vector2 mousePosition) {
 		var mouseOverDeckArea = DeckArea.GetRect().HasPoint(mousePosition);
 		var mouseOverInventoryArea = InventoryArea.GetRect().HasPoint(mousePosition);
-		var mouseOverStashArea = false;
+		var mouseOverStashArea = StashArea.GetRect().HasPoint(mousePosition);
 		
 		if (mouseOverDeckArea && _draggedCardOrigin != CardOrigin.Deck) {
 			if (_draggedCardState != DraggedCardState.OverDeckArea) {
@@ -177,7 +155,7 @@ public partial class Inventory : Control {
 	private void OnCardDragEndEventHandler(CardView view, Vector2 mousePosition) {
 		var mouseOverDeckArea = DeckArea.GetRect().HasPoint(mousePosition);
 		var mouseOverInventoryArea = InventoryArea.GetRect().HasPoint(mousePosition);
-		var mouseOverStashArea = false;
+		var mouseOverStashArea = StashArea.GetRect().HasPoint(mousePosition);
 		
 		if (mouseOverDeckArea && _draggedCardOrigin != CardOrigin.Deck) {
 			RemoveDraggedCardFromItsOrigin(view.Card);
